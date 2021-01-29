3d122c9a711cea594f9be45dea611774e88d88eb
Phu Nguyen <nhp2132@gmail.com>
2021-01-29 11:14:47 +0700

    29-1-2021

--- routes/Component/UpdateImage/UploadMultiple.js
+++ routes/Component/UpdateImage/UploadMultiple.js
@@ -17,24 +17,9 @@ router.post('/multiple',upload.array("upload_file_input",6),async (req,res,next)
         console.log("dong 1" + file.path)
     });
     console.log("dong 2" + temPath.path)
-    // res.json({temPath})
-    // var pathSave = path.join(__dirname,'../../../public/images/image.jpg')
-    // // const fileGettingUploaded = path.join(__dirname, "../../../public/images/Screenshot.png");
-
-  // const result =  MethodUploadFile.MethodUploadFilemMultiple(res,temPath);
-  //   console.log(result)
-    // cloudinary.uploader.upload('image.jpg')
-   
-
-    // console.log("dong 2" +pathSave)
-    // try{
-    //     fs.renameSync(temPath,pathSave);
-    // }
-    // catch(err){
-    //     console.log(err);
-    // }
     
-    // res.json('senfile succsessfull' + pathSave);
+
+    
  
 })
 
